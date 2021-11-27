2474f0f21ab16aed1ef6d1d2b178e6482ec232ff
Giorgio Pizzati <giorgio.pizzati@gmail.com>
2021-11-27 23:43:02 +0100

    final version

--- script.js
+++ script.js
@@ -25,8 +25,8 @@ canvas.height = r.height;
 
 
 
-var tolerance = (canvas.width+canvas.height)/1000;
-// var tolerance = 0;
+// var tolerance = (canvas.width+canvas.height)/1000;
+var tolerance = 0;
 
 // class Position {
 // 	constructor (x, y) {
@@ -44,7 +44,7 @@ var colorsForTeams = {
 } 
 
 class Obj {
-	constructor(team, x, y, shape, side, velocity, dirx, diry, life, defense, attack) {
+	constructor(team, x, y, shape, side, velocity, dirx, diry) {
 		this.team = team;
 		this.x = x;
 		this.y = y;
@@ -53,14 +53,11 @@ class Obj {
 		this.velocity = velocity;
 		this.dirx = dirx;
 		this.diry = diry;
-		this.life = life;
-		this.defense = defense;
-		this.attack = attack;
 	}
-	move() {
-			this.x += this.velocity * this.dirx ;
-			this.y += this.velocity * this.diry ;
-			// this.draw();
+	move(t) {
+			this.x += this.velocity * this.dirx * t;
+			this.y += this.velocity * this.diry * t;
+			this.draw();
 	}
 	draw() {
 		ctx.beginPath();
@@ -80,33 +77,19 @@ class Obj {
 	}
 }
 
-class Vector {
-	constructor(x,y){
-		this.x = x;
-		this.y = y;
-	}
-	norm(){
-		var n = Math.sqrt(Math.pow(this.x,2)+Math.pow(this.y,2));
-		this.x /= n;
-		this.y /= n;
-	}
-}
 
 class ObjFollower {
-	constructor(team, x, y, shape, side, velocity, life, defense, attack) {
+	constructor(team, x, y, shape, side, velocity) {
 		this.team = team;
 		this.x = x;
 		this.y = y;
 		this.shape = shape;
 		this.side = side;
 		this.velocity = velocity;
-		this.life = life;
-		this.defense = defense;
-		this.attack = attack;
 	}
 	move(objects){
-			var objs = objects.filter((obj) => obj !== this);
-			var objsDistances = objs.map((obj) => {
+			// var objs = objects.filter((obj) => obj.team !== this.team);
+			var objsDistances = objects.map((obj) => {
 				return {
 					obj: obj,
 					distance: Math.sqrt(
@@ -132,51 +115,12 @@ class ObjFollower {
 			) {
 				var nearest = nearests[0];
 				// if (this.shape==='ball' && nearest.obj.shape ==='ball' ||
-				// var otherSide =
-				// 	nearest.obj.shape === "ball"
-				// 		? nearest.obj.side
-				// 		: (Math.sqrt(2) * nearest.obj.side) / 2;
-				if ( overlap(this, nearest.obj) || collision(this, nearest.obj) ) {
-					// this.draw();
-					// console.log(nearest.distance)
-					// console.log("collision")
-					if (nearest.obj.team !== this.team){
-						fight(nearest.obj, this);
-					}
-					else{
-						// should circumnavigate my teammate in order to get near the enemy (not easy)
-						var newPos = {...this};
-
-						var dir = new Vector(nearest.obj.x-this.x, nearest.obj.y-this.y);
-						// var dir = new Vector(Math.random(), Math.random());
-						dir.norm();
-						newPos.x -= dir.x*this.velocity;
-						newPos.y -= dir.y*this.velocity;
-
-						// newPos.x += Math.random()*this.velocity;
-						// newPos.y += Math.random()*this.velocity;
-						// console.log(objs.filter(obj2=> overlap(newPos, obj2)))
-						if (objs.filter(obj2=> overlap(newPos, obj2)).length===0){
-							// clearInterval(interval)
-								console.log("moved");
-								this.x = newPos.x;
-								this.y = newPos.y;
-						}
-						else{
-						var iter=0
-						while (objs.filter(obj2=> overlap(newPos, obj2)).length!==0 && iter<=100){
-						// 	console.log("New pos");
-						var dir = new Vector(Math.random(), Math.random());
-						dir.norm();
-						newPos.x = this.x + dir.x*this.velocity;
-						newPos.y = this.y + dir.y*this.velocity;
-						// 	// newPos.x += dir.y*this.velocity;
-						// 	// newPos.y += -dir.x*this.velocity;
-						}
-					   console.log("moved");
-						this.x = newPos.x;
-						this.y = newPos.y;
-					}
+				var otherSide =
+					nearest.obj.shape === "ball"
+						? nearest.obj.side
+						: (Math.sqrt(2) * nearest.obj.side) / 2;
+				if (nearest.distance <= this.side + otherSide + tolerance) {
+					this.draw();
 					return;
 				}
 	// 			 if (this.shape === "ball" && nearest.obj.shape === "rect")
@@ -227,7 +171,7 @@ class ObjFollower {
 	// }		
 				var nearestsToFollow = nearests.filter((element) => element.obj.team !== this.team);
 				if (nearestsToFollow.length!==0){
-					// console.log(nearestsToFollow)
+					console.log(nearestsToFollow)
 					var nearest = nearestsToFollow[0];
 					var dirx = nearest.obj.x - this.x;
 					var diry = nearest.obj.y - this.y;
@@ -241,6 +185,8 @@ class ObjFollower {
 				
 			
 			}
+			this.draw();
+		
 	}
 	draw() {
 		ctx.beginPath();
@@ -255,43 +201,6 @@ class ObjFollower {
 	}
 }
 
-
-function overlap(obj1, obj2){
-	var distance = Math.sqrt(
-		Math.pow(obj1.x - obj2.x, 2) +
-			Math.pow(obj1.y - obj2.y, 2));
-	var otherSide =
-	obj2.shape === "ball"
-			? obj2.side
-			: (Math.sqrt(2) * obj2.side) / 2;
-	if (distance < obj1.side + otherSide ) {
-		return true;
-	}
-	return false;
-}
-function collision(obj1, obj2){
-	var distance = Math.sqrt(
-		Math.pow(obj1.x - obj2.x, 2) +
-			Math.pow(obj1.y - obj2.y, 2));
-	var otherSide =
-	obj2.shape === "ball"
-			? obj2.side
-			: (Math.sqrt(2) * obj2.side) / 2;
-	if (distance+tolerance <= obj1.side + otherSide ) {
-		return true;
-	}
-	return false;
-}
-
-function fight(obj1, obj2){
-	if (Math.random()>0.5){
-		// obj1 attacks obj2
-		obj2.life -= obj1.attack/obj2.defense;
-	}
-	else{
-		obj1.life -= obj2.attack/obj1.defense;
-	}
-}
 
 // function collisionDistance(obj1, obj2){
 // 	// WE DON'T WANT OBJECT OVERLAPPING!
@@ -344,7 +253,7 @@ function fight(obj1, obj2){
 function destroy(objects){
 	var toBeRemoved = [];
 	objects.forEach(obj=>{
-		if (obj.x>canvas.width || obj.x<0 || obj.y>canvas.height || obj.y<0 || obj.life<=0){
+		if (obj.x>canvas.width || obj.x<0 || obj.y>canvas.height || obj.y<0){
 			toBeRemoved.push(obj);
 		}
 	})
@@ -367,12 +276,12 @@ function main() {
 	var size = ((canvas.width+canvas.height)/2)/14;
 	var objects = [];
 	const velocity = 1;
-	objects.push(new Obj(0, offsetX, 100, "rect", size, 0, 0, 1, 1000, 10, 5));
-	objects.push(new Obj(0, offsetX + posX, 100, "rect", size, 0, 1, 1, 1000, 10, 5));
-	objects.push(new Obj(0, offsetX + 2 * posX, 100, "rect", size, 0, 1, 1, 1000, 10, 5));
-	objects.push(new Obj(1, offsetX, posY, "rect", size, 0, 0, 1, 1000, 10, 5));
-	objects.push(new Obj(1, offsetX + posX, posY, "rect", size, 0, 1, 1, 1000, 10, 5));
-	objects.push(new Obj(1, offsetX + 2 * posX, posY, "rect", size, 0, 1, 1, 1000, 10, 5));
+	objects.push(new Obj(0, offsetX, 100, "rect", size, 0, 0, 1));
+	objects.push(new Obj(0, offsetX + posX, 100, "rect", size, 0, 1, 1));
+	objects.push(new Obj(0, offsetX + 2 * posX, 100, "rect", size, 0, 1, 1));
+	objects.push(new Obj(1, offsetX, posY, "rect", size, 0, 0, 1));
+	objects.push(new Obj(1, offsetX + posX, posY, "rect", size, 0, 1, 1));
+	objects.push(new Obj(1, offsetX + 2 * posX, posY, "rect", size, 0, 1, 1));
 
 	var newPosX = 0;
 	var newPosY = 0;
@@ -393,13 +302,13 @@ function main() {
 		if (team ===0){
 			team = 1;
 			objects.push(
-				new ObjFollower(0, newPosX, newPosY, "ball", size / 5, velocity, 100, 20, 1)
+				new ObjFollower(0, newPosX, newPosY, "ball", size / 5, velocity)
 			);
 		}
 		else{
 			team = 0;
 			objects.push(
-				new ObjFollower(1, newPosX, newPosY, "ball", size / 5, velocity, 100, 20, 1)
+				new ObjFollower(1, newPosX, newPosY, "ball", size / 5, velocity)
 			);
 		}
 	}
@@ -423,11 +332,10 @@ function main() {
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
 		objects.forEach((element) => {
 			if (element instanceof ObjFollower) {
-				element.move(objects);
+				element.move(t, objects);
 			} else {
-				element.move();
+				element.move(t);
 			}
-			element.draw();
 		});
 		objects = destroy(objects);
 		// if (objects.length===0){
